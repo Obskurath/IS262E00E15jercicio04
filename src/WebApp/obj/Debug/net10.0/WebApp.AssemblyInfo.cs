69a3bf6829e2accb214c84039438497e146052ed
Eduardo Valencia <EduardoVG@proton.me>
2026-02-13 00:34:35 -0600

    Refactor polygon calculations and enhance styling in Index.cshtml

--- src/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ src/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae66487e6307cf67cec5545ffe2783d821e75591")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
